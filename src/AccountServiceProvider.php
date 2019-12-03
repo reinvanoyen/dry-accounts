6ea06bb75aa70628e4e708c27e1da5cbbf149556
Dieter <dieter@tnt.be>
2019-12-03 16:51:29 +0100

    Added config.

--- src/AccountServiceProvider.php
+++ src/AccountServiceProvider.php
@@ -11,15 +11,15 @@ use Tnt\ExternalApi\Facade\Api;
 
 class AccountServiceProvider extends ServiceProvider
 {
-	public function boot(ContainerInterface $app)
-	{
-		Api::get('1', 'authenticate', AuthController::class, 'authenticate');
-		Api::get('1', 'authorize', AuthController::class, 'authorize');
-	}
+    public function boot(ContainerInterface $app)
+    {
+        Api::get('1', 'authenticate', AuthController::class, 'authenticate');
+        Api::get('1', 'authorize', AuthController::class, 'authorize');
+    }
 
-	public function register(ContainerInterface $app)
-	{
-		$app->set(UserStorageInterface::class, SessionUserStorage::class);
-		$app->singleton(AuthenticationInterface::class, Authentication::class);
-	}
+    public function register(ContainerInterface $app)
+    {
+        $app->set(UserStorageInterface::class, SessionUserStorage::class);
+        $app->singleton(AuthenticationInterface::class, Authentication::class);
+    }
 }
